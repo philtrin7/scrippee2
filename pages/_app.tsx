7705b86d2e9847d7f464fdcae5ea1373abff9f74
Philtrin <Philtrin@Ubuntu>
2019-10-10 20:44:26 +1100

    Added global styles from Swipe theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,14 @@ import React from 'react'
 import { ApolloProvider } from '@apollo/react-hooks'
 import { withApollo } from '../lib/apollo'
 
+import GlobalStyles from './GlobalStyles'
+
 class MyApp extends App<any> {
   render() {
     const { Component, pageProps, apolloClient } = this.props
     return (
       <ApolloProvider client={apolloClient}>
+        <GlobalStyles />
         <Component {...pageProps} />
       </ApolloProvider>
     )
