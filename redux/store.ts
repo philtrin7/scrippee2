7d107d50b1af344163fd18295c6ba62b06e70b97
Philtrin <Philtrin@Ubuntu>
2019-10-11 22:03:35 +1100

    Added redux-logger to middleware

--- redux/store.ts
+++ redux/store.ts
@@ -1,5 +1,6 @@
 import { applyMiddleware, createStore } from 'redux'
 import createSagaMiddleware from 'redux-saga'
+import logger from 'redux-logger'
 
 import rootReducer from './rootReducer'
 import rootSaga from './rootSaga'
@@ -7,7 +8,7 @@ import rootSaga from './rootSaga'
 const bindMiddleware = (middleware: any) => {
   if (process.env.NODE_ENV !== 'production') {
     const { composeWithDevTools } = require('redux-devtools-extension')
-    return composeWithDevTools(applyMiddleware(...middleware))
+    return composeWithDevTools(applyMiddleware(...middleware, logger))
   }
   return applyMiddleware(...middleware)
 }
