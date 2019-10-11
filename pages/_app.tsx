bd254d5e69c49113b483a2b634b21e504b19b618
Philtrin <Philtrin@Ubuntu>
2019-10-11 21:55:39 +1100

    Setup redux/sagas based loosely on nextjs example (with-apollo-and-redux-saga)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,27 @@
 import App from 'next/app'
 import React from 'react'
+import { Provider } from 'react-redux'
 import { ApolloProvider } from '@apollo/react-hooks'
 import { withApollo } from '../lib/apollo'
+import withReduxSaga from 'next-redux-saga'
+import withRedux from 'next-redux-wrapper'
+import createStore from '../lib/store'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import GlobalStyles from './GlobalStyles'
 
 class MyApp extends App<any> {
   render() {
-    const { Component, pageProps, apolloClient } = this.props
+    const { Component, pageProps, apolloClient, store } = this.props
     return (
       <ApolloProvider client={apolloClient}>
-        <GlobalStyles />
-        <Component {...pageProps} />
+        <Provider store={store}>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </Provider>
       </ApolloProvider>
     )
   }
 }
 
-export default withApollo(MyApp)
+export default withRedux(createStore)(withReduxSaga(withApollo(MyApp)))
