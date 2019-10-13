f38009fff3681856e56a0b561998a460e11ef534
Philtrin <Philtrin@Ubuntu>
2019-10-13 11:35:41 +1100

    Setup redux based loosely on nextjs example

---
+++ redux/store.ts
@@ -0,0 +1,19 @@
+import { applyMiddleware, createStore } from 'redux'
+
+import rootReducer from './rootReducer'
+
+const bindMiddleware = (middleware: any) => {
+  if (process.env.NODE_ENV !== 'production') {
+    const { composeWithDevTools } = require('redux-devtools-extension')
+    return composeWithDevTools(applyMiddleware(...middleware))
+  }
+  return applyMiddleware(...middleware)
+}
+
+function configureStore(initialState: any) {
+  const store: any = createStore(rootReducer, initialState, bindMiddleware([]))
+
+  return store
+}
+
+export default configureStore
