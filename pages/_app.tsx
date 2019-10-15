089dd2570653a6939d14772cf1a83efc84df7ed4
Philtrin <Philtrin@Ubuntu>
2019-10-15 11:05:04 +1100

    Implemented basic redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,28 @@
 import App from 'next/app'
 import React from 'react'
+
 import { ApolloProvider } from '@apollo/react-hooks'
 import { withApollo } from '../lib/apollo'
+
+import { Provider } from 'react-redux'
+import withRedux from 'next-redux-wrapper'
+import createStore from '../redux/store'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import GlobalStyles from './GlobalStyles'
 
 class MyApp extends App<any> {
   render() {
-    const { Component, pageProps, apolloClient } = this.props
+    const { Component, pageProps, apolloClient, store } = this.props
     return (
       <ApolloProvider client={apolloClient}>
-        <GlobalStyles />
-        <Component {...pageProps} />
+        <Provider store={store}>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </Provider>
       </ApolloProvider>
     )
   }
 }
 
-export default withApollo(MyApp)
+export default withRedux(createStore)(withApollo(MyApp))
