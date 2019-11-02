8a34063d7d6e9e1a861fe09c5f0fd753bf2ca73c
Philtrin <Philtrin@Ubuntu>
2019-11-02 20:11:22 +1100

    Parted out convo from viewer into it's own component

---
+++ components/viewer/convo/convo.component.tsx
@@ -0,0 +1,137 @@
+import React from 'react'
+import ReactSVG from 'react-svg'
+
+import SwipeLogo from '../../../static/img/swipe-logo-replace-me.png'
+
+import convoViewerStyles from './convo.styles.scss'
+
+interface Props {}
+
+const ConvoViewer: React.FC<Props> = () => {
+  return (
+    <div>
+      <div className="middle">
+        <div className="container">
+          <ul>
+            <li>
+              <img src={SwipeLogo} alt="avatar" />
+              <div className="content">
+                <div className="message">
+                  <div className="bubble">
+                    <p>
+                      Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry.
+                    </p>
+                  </div>
+                </div>
+                <span>07:30am</span>
+              </div>
+            </li>
+            <li>
+              <div className="content">
+                <div className="message">
+                  <div className="bubble">
+                    <p>Many desktop publishing packages.</p>
+                  </div>
+                </div>
+                <span>11:56am</span>
+              </div>
+            </li>
+            <li>
+              <img src={SwipeLogo} alt="avatar" />
+              <div className="content">
+                <div className="message">
+                  <div className="bubble">
+                    <div className="attachment">
+                      <a href="/" className="round">
+                        <ReactSVG
+                          beforeInjection={(svg) => {
+                            svg.setAttribute(
+                              'style',
+                              'width: 20px; height: 20px'
+                            )
+                          }}
+                          src="/static/img/svg/file-text.svg"
+                        />
+                      </a>
+                      <div className="meta">
+                        <a href="/">
+                          <h5>build-plugins.js</h5>
+                        </a>
+                        <span>3kb</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <span>01:03pm</span>
+              </div>
+            </li>
+            <li>
+              <div className="content">
+                <div className="message">
+                  <div className="bubble">
+                    <p>It was popularised in the 1960s.</p>
+                  </div>
+                </div>
+                <span>05:42pm</span>
+              </div>
+            </li>
+            <li>
+              <img src={SwipeLogo} alt="avatar" />
+              <div className="content">
+                <div className="message">
+                  <div className="bubble">
+                    <p>
+                      It is a long established fact that a reader will be
+                      distracted.
+                    </p>
+                  </div>
+                </div>
+                <span>08:20pm</span>
+              </div>
+            </li>
+            <li>
+              <div className="content">
+                <div className="message">
+                  <div className="bubble">
+                    <p>
+                      Contrary to popular belief, Lorem Ipsum is not simply
+                      random text.
+                    </p>
+                  </div>
+                </div>
+                <span>
+                  10:15pm
+                  <ReactSVG
+                    beforeInjection={(svg) => {
+                      svg.setAttribute('style', 'width: 20px; height: 20px')
+                    }}
+                    wrapper="span"
+                    src="/static/img/svg/double-tick.svg"
+                  />
+                </span>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="container">
+        <div className="bottom">
+          <form>
+            <textarea
+              className="form-control"
+              placeholder="Type message..."
+              rows={1}
+            ></textarea>
+            <button type="submit" className="btn prepend">
+              <ReactSVG src="/static/img/svg/send-plane.svg" />
+            </button>
+          </form>
+        </div>
+      </div>
+      <style jsx>{convoViewerStyles}</style>
+    </div>
+  )
+}
+
+export default ConvoViewer
