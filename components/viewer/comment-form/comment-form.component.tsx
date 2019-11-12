6bbfa27cb956785deb2bd3ebae2acfbfe46c1aa8
Philtrin <Philtrin@Ubuntu>
2019-11-12 11:38:02 +1100

    Set up basic Formik form for adding comments

--- components/viewer/comment-form/comment-form.component.tsx
+++ components/viewer/comment-form/comment-form.component.tsx
@@ -1,23 +1,44 @@
 import React from "react";
 import ReactSVG from "react-svg";
 
+import { Formik, Form, Field } from "formik";
+import { Comment } from "../../../generated/graphql";
+import ClipLoader from "react-spinners/ClipLoader";
+
 import commentFormStyles from "./comment-form.styles.scss";
+
+type CommentForm = Pick<Comment, "text">;
 
 interface Props {}
 
 const CommentForm: React.FC<Props> = () => {
+  const handleSubmit = () => {};
+
   return (
     <div className="bottom">
-      <form>
-        <textarea
-          className="form-control"
-          placeholder="Type message..."
-          rows={1}
-        ></textarea>
-        <button type="submit" className="btn prepend">
-          <ReactSVG src="/static/img/svg/send-plane.svg" />
-        </button>
-      </form>
+      <Formik<CommentForm>
+        initialValues={{
+          text: ""
+        }}
+        onSubmit={_data => handleSubmit()}
+        validateOnBlur={false}
+        validateOnChange={false}
+      >
+        {() => (
+          <Form className="test">
+            <Field
+              className="form-control"
+              name="text"
+              placeholder="Add a comment"
+            />
+            <button type="submit" className="btn prepend">
+              <ClipLoader color={"#bdbac2"} size={21} />
+              <ReactSVG src="/static/img/svg/send-plane.svg" />
+            </button>
+          </Form>
+        )}
+      </Formik>
+
       <style jsx>{commentFormStyles}</style>
     </div>
   );
